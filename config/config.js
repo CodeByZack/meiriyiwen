275ad0e4bbeaf762464018e567cb04c68516374e
zackdk <958059970@qq.com>
2018-02-14 22:52:40 +0800

    bug

--- config/config.js
+++ config/config.js
@@ -5,6 +5,6 @@ module.exports = {
       key: 'myblog',
       maxAge: 2592000000
     },
-    mongodb: 'mongodb://localhost/test',
+    mongodb: 'mongodb://localhost/empty',
     qiniu: 'http://oztlwbo2e.bkt.clouddn.com/'
   }
